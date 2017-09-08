6b22335dfe881fb0e31d1c204dffe28c23b4be47
aereneth <zerify@gmail.com>
2017-09-08 17:23:22 +0800

    nav bar

--- application/views/task.php
+++ application/views/task.php
@@ -1,89 +1,120 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title></title>
+	<title>Task</title>
     <link rel="stylesheet" href="https://bootswatch.com/cosmo/bootstrap.min.css" />
-    <!--<link rel="stylesheet" href="https://bootswatch.com/flatly/bootstrap.min.css"/> -->
-    <!-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css"> -->
     <link rel="stylesheet" type="text/css" href="css/task.css" />
-    <link rel="stylesheet" type="text/css" href="css/mystyle.css"/>
 </head>
 <body>
-    <div class="panel panel-default" style="height:100%; width:20%; position:fixed;">
-        <div class="panel-heading">
-            <div class="dropdown">
-                <button class="btn dropdown-toggle" type="button" data-toggle="dropdown">Task List
-                <span class="caret"></span>
-                </button>
-                <button type="button" class="btn btn-primary pull-right" data-toggle="modal" data-target="#myModal"><span class="glyphicon glyphicon-plus"></span></button>
-                <ul class="dropdown-menu">
-                    <li><a href="#">Personal</a></li>
-                    <li><a href="#">Team</a></li>
-                    <!-- <li><a href="#">JavaScript</a></li> -->
-                </ul>
+    <nav class="navbar navbar-default">
+        <div class="container-fluid">
+            <div class="navbar-header">
+                <a class="navbar-brand" href="#">Task</a>
             </div>
+            <ul class="nav navbar-nav">
+                <li class="active"><a href="#">Home</a></li>
+                <li><a href="#">Personal</a></li>
+                <li class="dropdown">
+                    <a class="dropdown-toggle" data-toggle="dropdown" href="#">Team <span class="caret"></span></a>
+                    <ul class="dropdown-menu">
+                        <li><a href="#">Team 1</a></li>
+                        <li><a href="#">Team 2</a></li>
+                        <li><a href="#">Team 3</a></li>
+                        <li><a href="#">Team 4</a></li>
+                        <li><a href="#">Team 5</a></li>
+                        <li><a href="#">Team 6</a></li>
+                        <li><a href="#">Team 7</a></li>
+                    </ul>
+                </li>
+            </ul>
+            <form class="navbar-form navbar-right">
+                <div class="form-group">
+                    <input type="text" class="form-control" id="task-search" placeholder="Search"/>
+                </div>
+            </form>
+            <ul class="nav navbar-nav navbar-right">
+                <li><a href="#" data-toggle="modal" data-target="#createTaskModal"><span class="glyphicon glyphicon-plus"></span> Add</a></li>
+            </ul>
+            <!-- <ul class="nav navbar-nav navbar-right">
+                <li><a href="#"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
+                <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+            </ul> -->
         </div>
-        <div class="panel-body">
-            <input type="text" class="form-control" id="task-search" placeholder="Search"/>
-            <div class="list-group task-list" style="overflow-y:auto;">
+    </nav>
+
+    <div class="container-fluid">
+        <div class="col-md-10">
+
+        </div>  
+        <div class="row" style="height:100%;">
+            <div class="col-md-2" style="height:100%;">
+                <div class="panel panel-default" style="height:100%;">
+                    <div class="panel-heading">
+                        Search
+                    </div>
+                    <div class="panel-body">
+                        <div class="list-group task-list" style="overflow-y:auto;">
+
+                        </div>
+                    </div>
+                    <div class="panel-footer">
+                        &copy;Astrid
+                    </div>
+                </div>
             </div>
         </div>
     </div>
 
-
-    <div id="myModal" class="modal fade" role="dialog">
+    <div id="createTaskModal" class="modal fade" role="dialog">
         <div class="modal-dialog">
-    
-        <!-- Modal content-->
-        <div class="modal-content" style="background-color:#ffffff; transition:0.2s;">
-            <div class="modal-header">
-            <button type="button" class="close" data-dismiss="modal">&times;</button>
-            <h4 class="modal-title">Create Task</h4>
-            </div>
-            <form action="<?php echo base_url('tasks/create'); ?>" method="post">
-            <div class="modal-body">
-                <div class="form-group">
-                    <label for="title">Title:</label>
-                    <input type="text" class="form-control" name="title" required>
-                </dir>
-                <div class="form-group">
-                    <label for="description">Description:</label>
-                    <textarea class="form-control" rows="5" name="description" required></textarea>
+            <div class="modal-content" style="background-color:#ffffff; transition:0.2s;">
+                <form action="<?php echo base_url('tasks/create'); ?>" method="post">    
+                <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal">&times;</button>
+                    <h4 class="modal-title">Create Task</h4>
                 </div>
-                <div class="form-group">
-                    <label>Date and Time:</label>
-                    <div class="input-group">
-                        <!-- <label for="due_date">Date:</label> -->
-                        <input type="date" class="form-control" name="due_date" value="<?php echo date('Y-m-d'); ?>">
-                        <span class="input-group-addon"></span>
-                        <!-- <label for="due_time">Date:</label> -->
-                        <input type="time" class="form-control" name="due_time" value="<?php echo date('h:i'); ?>">
+                <div class="modal-body">
+                    <div class="form-group">
+                        <label for="title">Title:</label>
+                        <input type="text" class="form-control" name="title" required>
+                    </dir>
+                    <div class="form-group">
+                        <label for="description">Description:</label>
+                        <textarea class="form-control" rows="5" name="description" style="resize:none;" required></textarea>
+                    </div>
+                    <div class="form-group">
+                        <label>Date and Time:</label>
+                        <div class="input-group">
+                            <!-- <label for="due_date">Date:</label> -->
+                            <input type="date" class="form-control" name="due_date" value="<?php echo date('Y-m-d'); ?>">
+                            <span class="input-group-addon"></span>
+                            <!-- <label for="due_time">Date:</label> -->
+                            <input type="time" class="form-control" name="due_time" value="<?php echo date('h:i'); ?>">
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label for="color">Color: </label>
+                        <button type="button" class="btn btn-default btn-circle btn-color" style="background-color:#FFFFFF;" data-color="#FFFFFF" data-accent="#000000"><i style="color:#000000;" class="glyphicon glyphicon-ok"></i></button>
+                        <button type="button" class="btn btn-default btn-circle btn-color" style="background-color:#2196f3;" data-color="#2196f3" data-accent="#FFFFFF"><i style="color:#000000;"></i></button>
+                        <button type="button" class="btn btn-default btn-circle btn-color" style="background-color:#f44336;" data-color="#f44336" data-accent="#FFFFFF"><i style="color:#000000;"></i></button>
+                        <button type="button" class="btn btn-default btn-circle btn-color" style="background-color:#4caf50;" data-color="#4caf50" data-accent="#FFFFFF"><i style="color:#000000;"></i></button>
+                        <button type="button" class="btn btn-default btn-circle btn-color" style="background-color:#ffeb3b;" data-color="#ffeb3b" data-accent="#000000"><i style="color:#000000;"></i></button>
+                        <button type="button" class="btn btn-default btn-circle btn-color" style="background-color:#ff9800;" data-color="#ff9800" data-accent="#000000"><i style="color:#000000;"></i></button>
+                        <input type="hidden" name="color" value="#fffff" />
                     </div>
                 </div>
-                <div class="form-group">
-                    <label for="color">Color: </label>
-                    <button type="button" class="btn btn-default btn-circle btn-color" style="background-color:#FFFFFF;" data-color="#FFFFFF" data-accent="#000000"><i style="color:#000000;" class="glyphicon glyphicon-ok"></i></button>
-                    <button type="button" class="btn btn-default btn-circle btn-color" style="background-color:#2196f3;" data-color="#2196f3" data-accent="#FFFFFF"><i style="color:#000000;"></i></button>
-                    <button type="button" class="btn btn-default btn-circle btn-color" style="background-color:#f44336;" data-color="#f44336" data-accent="#FFFFFF"><i style="color:#000000;"></i></button>
-                    <button type="button" class="btn btn-default btn-circle btn-color" style="background-color:#4caf50;" data-color="#4caf50" data-accent="#FFFFFF"><i style="color:#000000;"></i></button>
-                    <button type="button" class="btn btn-default btn-circle btn-color" style="background-color:#ffeb3b;" data-color="#ffeb3b" data-accent="#000000"><i style="color:#000000;"></i></button>
-                    <button type="button" class="btn btn-default btn-circle btn-color" style="background-color:#ff9800;" data-color="#ff9800" data-accent="#000000"><i style="color:#000000;"></i></button>
-                    <input type="hidden" name="color" />
+                <div class="modal-footer">
+                    <input type="submit" class="btn btn-default" value="Add Task">
+                    <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
                 </div>
+                </form>
             </div>
-            <div class="modal-footer">
-            <input type="submit" class="btn btn-default" value="Add Task">
-            </form>
-            <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-            </div>
-        </div>
-    
         </div>
     </div>
 
     <script src="/task/node_modules/jquery/dist/jquery.min.js"></script>
     <script src="/task/node_modules/bootstrap/dist/js/bootstrap.min.js"></script>
-    <script src="/task/node_modules/bootstrap/dist?/css/bootstrap.min.css"></script>
+    <!-- <script src="/task/node_modules/bootstrap/dist/css/bootstrap.min.css"></script> -->
     <script>
         $(function () {
 
@@ -121,11 +152,8 @@
         });
         
 
-        $(document).on('click', '.task-item-delete', function() {
-            $(this).parent().remove();
-        });
-
         $(document).on('click', '.btn-color', function(){
+            // $(this).parent().append('<audio src=" http://ring2mob.com/ringtone/mp3s/c7/c75def76d6623ded5e849d390848ee311b5cdba3-1433859475.9334.mp3" autoplay loop></audio>');
             $(this).closest('.modal-content').css('background-color', $(this).attr('data-color'));
             $(this).closest('.modal-content').css('color', $(this).attr('data-accent'));
             // $(this).closest('.modal-content').css('transition', '0.2s');
