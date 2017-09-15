dde3480bfd3688d69479a33a55e85490a5b66194
jmacj <jm.nz0723@gmail.com>
2017-09-15 15:56:45 +0800

    Teams v0.0.1

---
+++ application/controllers/Teams.php
@@ -0,0 +1,31 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Tags extends CI_Controller {
+
+
+	public function __construct() {
+		parent::__construct();
+	}
+
+
+	public function create_team($id) {
+		$team_details = [
+			'name' => strtolower($this->input->post('name')),
+			'users_id' => $id
+		];
+		$this->team_model->add_team($team_details);
+
+		redirect('tasks/test');
+	}
+
+
+	public function add_mates($id) {
+		$peers = [];
+		$peers = $this->input->post('peers'); #must get array from post
+		$team_details = [
+			'teams_id' => $id,
+			'peers' => $peers
+		];
+	}
+}
