808cbe261d681a083486ce2623e8ec0766a0d5ca
Christian Jordan Dalan <cjdalan@gmail.com>
2017-11-24 20:19:15 +0800

    Fix base_url

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config["base_url"] = "http://note.payakapps.com/";
+$config["base_url"] = "http://task.payakapps.com/";
 
 /*
 |--------------------------------------------------------------------------
