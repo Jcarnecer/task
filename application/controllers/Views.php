bd7ae59d7f987d12d023269d63ca6434ad8f1d56
aereneth <zerify@gmail.com>
2018-02-22 10:17:18 +0800

    Autocomplete Complete

--- application/controllers/Views.php
+++ application/controllers/Views.php
@@ -26,6 +26,7 @@ class Views extends CI_Controller {
 		$data['projects']		= $this->project->get_many_by_user($this->session->user->id);
 		$data['colors']			= unserialize(COLORS);
 		$data['task_type']		= 'personal';
+		$data['company_user']	= $this->user_model->get_all();
 
 		$this->load->view('include/header', $data);
 		$this->load->view('include/modal', $data);
@@ -48,6 +49,7 @@ class Views extends CI_Controller {
 		$data['project'] 			= new stdClass();
 		$data['project']			= $this->project->get($id);
 		$data['project']->members 	= $this->project->get_members($id);
+		$data['company_user']		= $this->user_model->get_all();
 		
 		$this->load->view('include/header', $data);
 		$this->load->view('include/modal', $data);
